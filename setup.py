0dcbcea070cefa1bc2fcb9f23e7628880e3e1e94
Jake Stanger <jake.stanger@gmail.com>
2019-02-16 22:42:52 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Rofi-MPD',
+    version='1.0.0',
+    install_requires=['python-mpd2'],
+    py_modules=['rofi'],
+    url='https://github.com/JakeStanger/Rofi_MPD',
+    license='MIT',
+    author='Jake Stanger',
+    author_email='jakestanger@gmail.com',
+    description='A rofi menu for interacting with MPD.'
+)
