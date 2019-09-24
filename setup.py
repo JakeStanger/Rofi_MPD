55c4a7e1bccccede3dcaf3d677becf2185c63e8b
Jake Stanger <mail@jstanger.dev>
2019-09-24 21:25:13 +0100

    Actually call this V1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Rofi-MPD',
-    version='1.0.2',
+    version='1.1.0',
     install_requires=['python-mpd2', 'mutagen'],
     packages=['rofi', 'rofi_mpd'],
     # py_modules=['rofi', 'rofi-mpd'],
